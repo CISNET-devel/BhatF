0554f2ac194ef548f8bb6f29e31948623159c9ea
Alexander Gaenko <galexv@umich.edu>
2021-09-30 13:35:39 -0400

    Commented out unused test function

--- tests/testthat/test-dfp.R
+++ tests/testthat/test-dfp.R
@@ -1,17 +1,17 @@
-test_that("DFP with the built-in C++ function", {
-    x = list(label=c("A","B","C"),
-             est=c(10.,10.,.01),
-             low=c(0,0,0),
-             upp=c(100,20,.1))
+## test_that("DFP with the built-in C++ function", {
+##     x = list(label=c("A","B","C"),
+##              est=c(10.,10.,.01),
+##              low=c(0,0,0),
+##              upp=c(100,20,.1))
 
-    result = dfp(x,"test_fn")
+##     result = dfp(x,"test_fn")
     
-    expect_equal(result$fmin,-2.579809e+04)
-    expect_equal(result$est, c(2.093722e+01, 4.424903e-01, 4.627753e-02), tolerance=1e-5)
-    expect_equal(result$label, x$label)
-    expect_equal(result$status, 0)
-    expect_equal(result$nfcn, 164)
-})
+##     expect_equal(result$fmin,-2.579809e+04)
+##     expect_equal(result$est, c(2.093722e+01, 4.424903e-01, 4.627753e-02), tolerance=1e-5)
+##     expect_equal(result$label, x$label)
+##     expect_equal(result$status, 0)
+##     expect_equal(result$nfcn, 164)
+## })
 
 ## FIXME:TODO: test for "fixed" variables (logicals? integers?)
 ## FIXME:TODO: test for inconsistent array lengths
